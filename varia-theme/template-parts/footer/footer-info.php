9666fb8220a27d7b9e2ed10011478be72a19b0dd
frulecalvi <frulecalvi@gmail.com>
2026-04-27 13:03:33 -0300

    Subidos comprimidos de temas

--- varia-theme/template-parts/footer/footer-info.php
+++
@@ -1,5 +0,0 @@
-<?php declare( strict_types = 1 ); ?>
-<div class="site-info">
-	<?php get_template_part( 'template-parts/footer/site', 'name' ); ?>
-	<?php get_template_part( 'template-parts/footer/privacy', 'policy' ); ?>
-</div><!-- .site-info -->
